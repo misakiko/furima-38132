53b29ca55f966c99225440466481be6bf347081d
misakiko <34misaki.k@gmail.com>
2022-07-13 15:13:55 +0900

    モデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
 end
 
+# パスワード半角英数字
+# validates :カラム名(シンボルで指定),検証ルール（こちらもシンボルで指定）
+# /^[a-zA-Z0-9]+$/
 
-validates :nickname, presences: true
-validates :last_name, presences: true
-validates :first_name, presences: true
-validates :last_name_kana, presences: true
-validates :first_name_kana, presences: true
-validates :birthday, presences: true
-
-
-パスワード半角英数字
-validates :カラム名(シンボルで指定),検証ルール（こちらもシンボルで指定）
-/^[a-zA-Z0-9]+$/
