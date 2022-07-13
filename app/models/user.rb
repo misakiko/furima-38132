288e6de84e0dfc0557fd4d64cb5b722cc7de3230
misakiko <34misaki.k@gmail.com>
2022-07-13 14:15:01 +0900

    userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,3 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
+
+
+validates :nickname, presences: true
+validates :last_name, presences: true
+validates :first_name, presences: true
+validates :last_name_kana, presences: true
+validates :first_name_kana, presences: true
+validates :birthday, presences: true
+
+
+パスワード半角英数字
+validates :カラム名(シンボルで指定),検証ルール（こちらもシンボルで指定）
+/^[a-zA-Z0-9]+$/
