d953e541c27bd0817489b6a9addf83e5ad6d0d66
misakiko <34misaki.k@gmail.com>
2022-07-15 15:42:02 +0900

    FactoryBotの記述追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,19 +1,18 @@
 FactoryBot.define do
   factory :user do
-      person = Gimei.name
+    person = Gimei.name
 
     # 漢字、カナが生成されるgem
-    nickname {Faker::Name.name}
-    email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
+    nickname { Faker::Name.name }
+    email { Faker::Internet.free_email }
+    password { '1a' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
     last_name { person.last.kanji }
     first_name { person.first.kanji }
     last_name_kana { person.last.katakana }
     first_name_kana { person.first.katakana }
-    birthday {Faker::Date.between(from:'1930-01-01',to:'2017-12-31')}
+    birthday { Faker::Date.between(from: '1930-01-01', to: '2017-12-31') }
   end
 end
 
-# birthday {Faker::Date.between}
 # passwordに('1a'+Faker...)すると英数混合に
