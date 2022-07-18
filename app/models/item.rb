5ea370e9089521f5468ad1871010d933677438a8
misakiko <34misaki.k@gmail.com>
2022-07-18 16:54:32 +0900

    ActiveStorageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,7 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one :purchase
+  has_one_attached :image
 
   validates :item_name, presences: true
   validates :messages, presences: true
