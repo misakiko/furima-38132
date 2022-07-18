b85f38bfaf02ee70f2b38b83240f2fadfd1412f0
misakiko <34misaki.k@gmail.com>
2022-07-18 15:38:51 +0900

    itemテーブルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
-end
+
+  validates :item_name, presences: true
+  validates :messages, presences: true
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                    format: { with: /\A[0-9]+\z/ }
+end                  
+  
