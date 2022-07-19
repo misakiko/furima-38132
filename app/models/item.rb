46234ca475970483e91c92a1385793dd6511b8fb
misakiko <34misaki.k@gmail.com>
2022-07-19 21:35:34 +0900

    エラーの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,12 +12,14 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true
   validates :messages, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                    format: { with: /\A[0-9]+\z/ }
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                      presence: { message: "can't be blank" }
+  end
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :situation_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :price_bear_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :area_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :area_id, numericality: { other_than: 0, message: "can't be blank" } 
   validates :days_id, numericality: { other_than: 1, message: "can't be blank" } 
 end                  
   
