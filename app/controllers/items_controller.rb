243e72514b57162ac04ae06982823476c9ab29f4
misakiko <34misaki.k@gmail.com>
2022-07-20 14:27:59 +0900

    コントローラーのcreate修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,21 +3,24 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
   def new
     @item = Item.new
   end
 
   def create
     @item = Item.new(item_params)
-     if @item.save
-        redirect_to root_path
-     else
-       render :new
-     end
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
-    def item_params
-    params.require(:item).permit(:image, :item_name, :messages, :category_id, :situation_id, :price_bear_id, :area_id, :days_id, :price).merge(user_id: current_user.id)
-    end
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :messages, :category_id, :situation_id, :price_bear_id, :area_id, :days_id,
+                                 :price).merge(user_id: current_user.id)
+  end
 end
