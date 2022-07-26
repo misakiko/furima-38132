98948dfd9a42f0d97d7337d6c47ecb47f46380a9
misakiko <34misaki.k@gmail.com>
2022-07-26 12:16:02 +0900

    カラムなどの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,15 +1,16 @@
 class PurchasesController < ApplicationController
   def index
     @purchase = Order.new
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
   end
 
   def create
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
     @purchase = Order.new(purchase_params) 
     if @purchase.valid?
-        @purchase.save
-        redirect_to root_path
+      pay_item
+      @purchase.save
+      return redirect_to root_path
     else
         render 'index'
     end
@@ -17,8 +18,15 @@ class PurchasesController < ApplicationController
 
   private
     def purchase_params
-      params.permit(:card_number, :card_exp_month, :card_exp_month, :cvc, :post_cord, :area_id, :city, :block_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id]) 
+      params.require(:order).permit(:post_cord, :area_id, :city, :block_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token]) 
     end
+
+    def pay_item
+     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: purchase_params[:token],
+        currency: 'jpy' 
+      )   
+    end     
 end
-
-
