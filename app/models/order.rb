c6ab9807679dbd53ed626189b47d60f9bdc1834c
misakiko <34misaki.k@gmail.com>
2022-07-26 12:16:28 +0900

    バリデーションの追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,21 +1,18 @@
 class Order
   include ActiveModel::Model
-  attr_accessor :post_cord, :area_id, :city, :block_number, :building, :phone_number, :number, :cvc, :exp_month, :exp_year
-
-# addressのバリデーション
-  VALID_POSTAL_CODE_REGEX = /^[0-9]{3}-[0-9]{4}$/
-  validates :post_cord, presence: true, format: {with: VALID_POSTAL_CODE_REGEX }
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  attr_accessor :post_cord, :area_id, :city, :block_number, :building, :phone_number, :user_id, :item_id, :token
+  validates :post_cord, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }
   validates :area_id, presence: true
   validates :city, presence: true
   validates :block_number, presence: true
-  # (validates :building 空でもいいからいらないかも)
-  VALID_PHONE_NUMBER_REGEX = /^\d{10,11}$/
-  validates :phone_number, presence: true, format: {with: VALID_PHONE_NUMBER_REGEX }
-#  クレカのバリデーション必要？
+  validates :phone_number, presence: true, format: {with: /\A\d{10,11}\z/ }
+  validates :token, presence: true
 
-def save
-  order = Purchase.create(user_id: user_id, item_id: item_id)
-  Address.create(post_cord: post_cord, area_id: area_id, city: city, block_number: block_number, building: building, phone_number: phone_number, order_id: order.id)
+  validates :area_id, numericality: { other_than: 0, message: "can't be blank" }
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(post_cord: post_cord, area_id: area_id, city: city, block_number: block_number, building: building, phone_number: phone_number, purchase_id: purchase.id)
+  end
 end
- 
- 
