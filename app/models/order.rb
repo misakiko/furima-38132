d2c635bf85453b0b55c2207c27addeeb2d04dd80
misakiko <34misaki.k@gmail.com>
2022-07-27 17:29:30 +0900

    修正依頼のバリデーション変更

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,16 +3,16 @@ class Order
   extend ActiveHash::Associations::ActiveRecordExtensions
   attr_accessor :post_cord, :area_id, :city, :block_number, :building, :phone_number, :user_id, :item_id, :token
 
-  validates :post_cord, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-  validates :area_id, presence: true
-  validates :city, presence: true
-  validates :block_number, presence: true
-  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
-  validates :token, presence: true
-
-  validates :area_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :user_id, presence: true
-  validates :item_id, presence: true
+  with_options presence: true do
+    validates :post_cord, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :area_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :city
+    validates :block_number
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :token
+    validates :user_id
+    validates :item_id
+  end
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
